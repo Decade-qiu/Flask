82e29d3df4d1f579f56e0bd61a8b2457866858de
Decade-qiu <3237550600@qq.com>
2023-03-24 21:13:39 +0800

    add_whiteboard

--- static/js/white.js
+++ static/js/white.js
@@ -69,4 +69,93 @@ function joinRoom(roomUUID, roomToken) {
     });
 }
 
-// 把教具切换为「铅笔
+// 实现所有教具切换的代码
+var pencil = document.getElementById("pencil");
+var eraser = document.getElementById("eraser");
+var text = document.getElementById("text");
+var selector = document.getElementById("selector");
+var color = document.getElementById("color");
+var stroke = document.getElementById("stroke");
+var undo = document.getElementById("undo");
+var redo = document.getElementById("redo");
+var clear = document.getElementById("clear");
+var zoomIn = document.getElementById("zoom-in");
+var zoomOut = document.getElementById("zoom-out");
+var zoomReset = document.getElementById("zoom-reset");
+// 为所有教具绑定点击事件
+pencil.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "pencil"});
+});
+eraser.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "eraser"});
+});
+text.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "text"});
+});
+selector.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "selector"});
+});
+color.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "color"});
+});
+stroke.addEventListener("click", function() {
+    room.setMemberState({currentApplianceName: "stroke"});
+});
+undo.addEventListener("click", function() {
+    room.undoMagix();
+});
+redo.addEventListener("click", function() {
+    room.redoMagix();
+});
+clear.addEventListener("click", function() {
+    room.clear();
+});
+zoomIn.addEventListener("click", function() {
+    room.zoomIn();
+});
+zoomOut.addEventListener("click", function() {
+    room.zoomOut();
+});
+zoomReset.addEventListener("click", function() {
+    room.zoomReset();
+});
+// 实现颜色选择的代码
+var colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener("change", function() {
+    room.setMemberState({strokeColor: colorPicker.value});
+});
+// 实现笔触粗细选择的代码
+var strokePicker = document.getElementById("stroke-picker");
+strokePicker.addEventListener("change", function() {
+    room.setMemberState({strokeWidth: parseInt(strokePicker.value)});
+});
+// 实现文字输入的代码
+var textInput = document.getElementById("text-input");
+textInput.addEventListener("change", function() {
+    room.setMemberState({text: textInput.value});
+});
+// 实现文字大小选择的代码
+var textSizePicker = document.getElementById("text-size-picker");
+textSizePicker.addEventListener("change", function() {
+    room.setMemberState({fontSize: parseInt(textSizePicker.value)});
+});
+// 实现文字字体选择的代码
+var textFontPicker = document.getElementById("text-font-picker");
+textFontPicker.addEventListener("change", function() {
+    room.setMemberState({fontFamily: textFontPicker.value});
+});
+// 实现文字粗细选择的代码
+var textBoldPicker = document.getElementById("text-bold-picker");
+textBoldPicker.addEventListener("change", function() {
+    room.setMemberState({bold: textBoldPicker.value});
+});
+// 实现文字斜体选择的代码
+var textItalicPicker = document.getElementById("text-italic-picker");
+textItalicPicker.addEventListener("change", function() {
+    room.setMemberState({italic: textItalicPicker.value});
+});
+// 实现文字下划线选择的代码
+var textUnderlinePicker = document.getElementById("text-underline-picker");
+textUnderlinePicker.addEventListener("change", function() {
+    room.setMemberState({underline: textUnderlinePicker.value});
+});
